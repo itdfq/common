8f2eca2b1cefb421025ffbb8f4d294eb0b363504
杀阡陌 <fangqin.duan@ideacome.com>
2022-02-24 10:27:46 +0800

    基于Redisson的注解分布式锁

--- src/main/java/com/itdfq/common/pojo/Result.java
+++ src/main/java/com/itdfq/common/pojo/Result.java
@@ -1,52 +1,71 @@
 package com.itdfq.common.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.io.Serializable;
 
 /**
- * @author GodChin
- * @date 2022/2/18 16:23
- * @email 909256107@qq.com
+ * @author: GodChin
+ * @date: 2022/2/24 14:32
+ * @mark: Controller统一返回类
  */
-@Data
-@AllArgsConstructor
-public class Result {
-    /**
-     * 标识返回的状态码
-     */
-    private Integer code;
-    /**
-     * 标识返回的信息
-     */
-    private String message;
-    /**
-     * 标识返回的数据
-     */
-    private Object data;
-
+public class Result<T> implements Serializable {
     private static final int SUCCESS_CODE = 200;
     private static final int FAIL_CODE = 9999;
+    private String msg;
+    private Integer code;
+    private T data;
 
-    /**
-     * 私有化，防止new
-     */
-    private Result() {
+    public Result() {
+
+    }
+
+    public Result(String msg, Integer code, T data) {
+        this.msg = msg;
+        this.code = code;
+        this.data = data;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public String getMsg() {
+        return this.msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public static Result fail(String msg) {
+        return new Result(msg, FAIL_CODE, null);
+    }
+
+    public static Result success(Object data) {
+        return new Result<>(null, SUCCESS_CODE, data);
+    }
+
+    public static Result success() {
+        return new Result(null, SUCCESS_CODE, null);
+    }
+
+    public Boolean isSuccess() {
+        if (SUCCESS_CODE == code) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 
 
-    public static Result ok(Object data, String message) {
-        return new Result(SUCCESS_CODE, message, data);
-    }
-
-    public static Result ok(Object data) {
-        return Result.ok(data, "success");
-    }
-
-    public static Result error(String message) {
-        return new Result(FAIL_CODE, message, "");
-    }
-
-    public static Result error(Integer code, String message) {
-        return new Result(code, message, "");
-    }
 }
